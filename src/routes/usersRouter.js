4da249a89320c448ff61846fd01f7df1c96085bd
Alessio <belcastroalessio@outlook.it>
2024-01-17 23:18:18 +0100

    user model schema

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,5 +1,51 @@
-import express from "express";
+// Importing required modules
+import express from "express"
+import User from "../models/Users.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
-const usersRoute = express.Router();
+// Creating an instance of express router
+const usersRoute = express.Router()
 
-export default usersRoute;
+// Handling login route
+usersRoute
+    .post("/login", async (req, res) => {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(401).send("Invalid email or password")
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            return res.status(401).send("Invalid email or password")
+        }
+
+        const payload = { userId: user._id, name: user.name, email: user.email }
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "1h",
+        })
+        const response = { token: token, user: payload }
+
+        // Authentication successful
+        res.status(200).send(response)
+    })
+
+    // Handling register route
+    .post("/register", async (req, res) => {
+        const password = await bcrypt.hash(req.body.password, 10)
+        if (!password) {
+            res.status(400).send("Password is required")
+        }
+        const user = await User.findOne({ email: req.body.email })
+        if (user) {
+            res.status(400).send("User already exists")
+        }
+        const newUser = await User.create({ ...req.body, password: password })
+        res.status(201).send(newUser)
+    })
+
+// Exporting the usersRoute
+export default usersRoute
